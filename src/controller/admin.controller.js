72396753dc396732a17b0202a7c43112ca2b707e
harshit <iamharshitsharma518@gmail.com>
2026-01-23 14:42:08 +0530

    done with the admin routes

--- src/controller/admin.controller.js
+++ src/controller/admin.controller.js
@@ -1,26 +1,27 @@
-const userModel = require('../models/user.model')
+const adminModel = require('../models/admin.model')
 const bycrypt  = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+
 async function register(req,res){
     const {name,email,password} = req.body
-    const isuserexit = await userModel.findOne({email})
-    if(isuserexit){
-        res.status(400).json({message:"User Already Exists"})
+    const isadminexit = await adminModel.findOne({email})
+    if(isadminexit){
+        res.status(400).json({message:"Teacher Already Exists"})
     }
     const hashpassword = await bycrypt.hash(password,10)
-    const user = await userModel.create({
+    const admin = await adminModel.create({
         name,
         email,
         password:hashpassword
     })
-    const token = jwt.sign({id:user._id},process.env.JWTSECRET)
+    const token = jwt.sign({id:admin._id},process.env.JWTSECRET)
     res.cookie("token",token)
     res.status(201).json({
         message:"User registered successfully",
-        user:{
-            email:user.email,
-            _id:user._id,
-            name:user.name
+        admin:{
+            email:admin.email,
+            _id:admin._id,
+            name:admin.name
         }
     })
 }
@@ -29,14 +30,14 @@ async function register(req,res){
 async function login(req,res)
 {
     const{email,password} = req.body
-    const user = await userModel.findOne({email})
-    if(!user){
+    const admin = await adminModel.findOne({email})
+    if(!admin){
         return res.status(401).json({
             message:"Invalid Email Id"
         })
     }
 
-    const ispassword = await bycrypt.compare(password,user.password)
+    const ispassword = await bycrypt.compare(password,admin.password)
     if(!ispassword){
         return res.status(401).json({
             message:"Invalid password"
@@ -44,7 +45,7 @@ async function login(req,res)
     }
 
     return res.status(200).json({
-        message:"User Login Successfull"
+        message:"Admin Login Successfull"
     })
 }
 
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,4 +3,5 @@ const authcontroller = require('../controller/auth.controller')
 const router = express.Router()
 
 router.post("/register",authcontroller.register)
+router.get("/login",authcontroller.login)
 module.exports = router
