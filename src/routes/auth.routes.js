8ca5835a504412ed9c530868b7374d2be34ad76d
harshit <iamharshitsharma518@gmail.com>
2026-01-24 13:51:37 +0530

    done with the middleware usage and connectivity

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const authcontroller = require('../controller/auth.controller')
 const router = express.Router()
-
+const usercourse = require('../controller/usercourse.controller')
+const authuser = require('../middleware/auth.middleware')
 router.post("/register",authcontroller.register)
 router.get("/login",authcontroller.login)
+router.get('/showcourse',authuser,usercourse.seecourse)
 module.exports = router
